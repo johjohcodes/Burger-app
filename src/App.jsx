feabc38f68150922e9f00bb4b286a5b6146810d7
joejoh <georgeja.18@student.funaab.edu.ng>
2025-08-03 00:17:52 +0100

    okayyy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+function Home() {
+  return <h1>Welcome to Burger App!</h1>;
+}
+
+function About() {
+  return <h1>About Burger App</h1>;
+}
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to Burger App!</h1>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
